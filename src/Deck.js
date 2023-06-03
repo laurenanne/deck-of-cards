21e21f8121b06e24f9af3a6c32277ed0c0eeb2c1
lauren brady <lauren.a.brady@gmail.com>
2023-06-03 16:45:30 -0400

    Part2

--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card.js";
 import axios from "axios";
 
 function Deck() {
   const [deck, setDeck] = useState("");
-  const [card, setCard] = useState({ image: "", status: "" });
+  const [card, setCard] = useState({ image: "", status: undefined });
+  const [isDraw, setIsDraw] = useState(false);
+  const timerId = useRef();
 
   // this is called after component is first added to DOM
   // and every time deck is shuffled
@@ -21,6 +23,10 @@ function Deck() {
     [card.status]
   );
 
+  const toggleDraw = () => {
+    setIsDraw((isDraw) => !isDraw);
+  };
+
   //called on button click  async function fetchCard() {
   async function fetchCard() {
     const cardResult = await axios.get(
@@ -29,8 +35,8 @@ function Deck() {
 
     if (cardResult.data.error) {
       alert("Error: no cards remaining!");
-      setCard({ image: "" });
-      setCard({ status: "reload" });
+      setCard({ image: "", status: "reload" });
+      setIsDraw(false);
     } else {
       setCard({
         image: cardResult.data.cards[0].image,
@@ -38,12 +44,42 @@ function Deck() {
     }
   }
 
+  function setTimer() {
+    timerId.current = setInterval(() => {
+      fetchCard();
+    }, 1000);
+  }
+
+  function cleanUpTimer() {
+    clearInterval(timerId.current);
+  }
+
+  useEffect(() => {
+    if (isDraw) {
+      setTimer();
+    } else {
+      cleanUpTimer();
+    }
+  }, [isDraw]);
+
   return (
     <div>
       <div>
         <Card card={card} />
       </div>
-      <button onClick={fetchCard}>Get Card</button>
+      <button
+        style={{ visibility: isDraw ? "hidden" : "visible" }}
+        onClick={toggleDraw}
+      >
+        Start Drawing
+      </button>
+
+      <button
+        style={{ visibility: isDraw ? "visible" : "hidden" }}
+        onClick={toggleDraw}
+      >
+        Stop Drawing
+      </button>
     </div>
   );
 }
